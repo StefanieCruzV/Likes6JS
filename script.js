53f47d97a2a37be289552fc4ad444aa4cffc39f4
Stefanie Cruz <stefanie.cruz.vazquez@gmail.com>
2023-10-02 14:02:05 -0700

    Upload Local Changes

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var number = document.querySelector('#number');
+var count = 3;
+//console.log(number);
+
+function add(element){
+    count ++;
+     console.log(element.on);
+     //console.log(number);
+     number.innerText = count+ " ";
+}
